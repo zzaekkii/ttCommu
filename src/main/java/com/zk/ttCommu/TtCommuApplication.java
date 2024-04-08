55e2da3eb1f3b09cf4267b7146e8e4367fc16f46
zakie <realzzaekkii@gmail.com>
2024-04-08 19:11:01 +0900

    :tada: chore: spring initializr

---
+++ src/main/java/com/zk/ttCommu/TtCommuApplication.java
@@ -0,0 +1,13 @@
+package com.zk.ttCommu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtCommuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtCommuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zk/ttCommu/TtCommuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zk.ttCommu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtCommuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
